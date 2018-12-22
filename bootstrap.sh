60e188da5a7fc45a9688da25c0e8ee2fd0855582
VeryDampTowel <32224673+VeryDampTowel@users.noreply.github.com>
2018-12-22 13:47:04 -0500

    Added some Mac specific installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,13 +13,29 @@ function startUp() {
   source ~/.bash_profile;
 }
 
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	startUp;
+  startUp;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		startUp;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    startUp;
+  fi;
 fi;
-unset startU;
+
+
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+fi
+
+unset startUp;
---
+++ brew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install stuff with Homebrwe
+
+brew update
+
+brew upgrade
+
+brew install mas git vim
+brew cask install appcleaner bartender expressvpn mpv transmission balenaetcher
+brew cask install bitwarden flux spotmenu
+brew cask install firefox spotify macs-fan-control handbrake gpg-suite
